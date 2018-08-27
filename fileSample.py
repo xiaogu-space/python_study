da0c68c36246c607e1685a2917278e68b133d30b
gushoubang <gushoubang@dituwuyou.com>
2018-08-27 19:34:18 +0800

    支持读写文件

--- fileSample.py
+++ fileSample.py
@@ -1,17 +1,18 @@
 # coding:utf-8
 
-poen='''fdsafsadf
+poen = '''fdsafsadf
 afsdasdfsadf'''
 
-f=open('./fileString.text','w')
+# f=open('./fileString.text','w')
+f = open('./fileString.text', 'a')  #追加模式
 f.write(poen)
 f.close()
 
 # f=open('./fileSample.py')
-f=open('./fileString.text')
+f = open('./fileString.text')
 while True:
-    line=f.readline()
-    if(len(line))==0:
+    line = f.readline()
+    if (len(line)) == 0:
         break
     print(line)
 f.close()
---
+++ saveObject.py
@@ -0,0 +1,16 @@
+#完整的存储对象
+import pickle  #通过pickle可以将任何纯python对象存储到一个文件中，并在存储后将其取回
+import json
+
+file = './saveObject.json'
+f = open(file, 'wb')
+
+listObj = ['天气', '日期', '交通']
+
+pickle.dump(listObj, f)
+f.close()
+
+import json
+# outfile = open('saveData.json', 'w')
+with open('saveData.json', 'w') as outfile:
+    json.dump(listObj, outfile, ensure_ascii=False)#支持中文写入
